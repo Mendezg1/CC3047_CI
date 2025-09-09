98f251cc2c984d0410e2f9c8795de2841eedbc98
Mendezg1 <87867522+Mendezg1@users.noreply.github.com>
2025-09-09 07:50:29 -0600

    median

--- src/stats_lib.py
+++ src/stats_lib.py
@@ -1,4 +1,17 @@
-def media(nums):
+def mean(nums):
     if not nums:
-        raise ValueError("La lista no puede estar vacía")
+        raise ValueError("List is empty.")
     return sum(nums) / len(nums)
+
+def median(nums):
+    if not nums:
+        raise ValueError("List is empty.")
+    nums_sorted = sorted(nums)
+    n = len(nums)
+    mid = n // 2
+    if n % 2 == 0:
+        return (nums_sorted[mid - 1] + nums_sorted[mid]) / 2
+    else:
+        return nums_sorted[mid]
+    
+    
